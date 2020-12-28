72ba3cca7666333ed26f4691d3d2ba2ada42f567
Koppula Akshay Reddy <akshay@koppulas-air.home>
2020-12-28 14:14:22 -0600

    set up + connect to local git repo

---
+++ src/test/java/com/akshay/rest/webservices/spring/cloud/config/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.rest.webservices.spring.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
